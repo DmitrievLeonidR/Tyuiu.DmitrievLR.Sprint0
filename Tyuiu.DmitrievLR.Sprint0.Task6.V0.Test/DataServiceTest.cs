a7dc5b4250ee70bc7599bebf7fcda809fde53f4d
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-11-29 20:55:32 +0500

    Task6

---
+++ Tyuiu.DmitrievLR.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿namespace Tyuiu.DmitrievLR.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
