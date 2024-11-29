b6a96ec8e0055dc14a5883a9e987b42abc667c2f
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-11-29 18:15:19 +0500

    Task1

---
+++ Tyuiu.DmitrievLR.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Tyuiu.DmitrievLR.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //11
+            Console.WriteLine("Hello, World!");
+            
+            string name = "Леонид";
+            int age = 21;
+
+            Console.WriteLine("Здравствуйте, меня зовут " + name + ", мне " + age + " год." );
+
+            //end
+            Console.ReadKey();
+        }
+    }
+}
