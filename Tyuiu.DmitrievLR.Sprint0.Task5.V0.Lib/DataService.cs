b1bc6311c6c7780b5371d10e743c917ed3264076
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-11-29 20:29:26 +0500

    Task5

---
+++ Tyuiu.DmitrievLR.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace Tyuiu.DmitrievLR.Sprint0.Task5.V0.Lib
+{
+    public class DataService 
+    {
+        public static int Addiction(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Substraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Division(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine(" Переменная b = {0}, на ноль делить нельзя", b);
+                return -1;
+            }
+            else
+            {
+                return a / b;
+            }
+
+        }
+
+    }
+}
---
+++ Tyuiu.DmitrievLR.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.DmitrievLR.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.DmitrievLR.Sprint0.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("A + B = " + DataService.Addiction(1, 5));
+            Console.WriteLine("A - B = " + DataService.Substraction(15, 5));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+            Console.WriteLine("A / B = " + DataService.Division(5, 0));
+
+    
+            Console.ReadKey();
+        }
+    }
+}
