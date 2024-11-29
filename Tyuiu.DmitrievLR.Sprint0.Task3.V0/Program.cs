a1eaf343f0f78e974195cbf7486ec47046b046dc
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-11-29 19:57:09 +0500

    Task3

---
+++ Tyuiu.DmitrievLR.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.DmitrievLR.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.DmitrievLR.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(DataService.Sum(7,7));
+
+            Console.ReadKey();
+        }
+    }
+}
