c21f0d242596861383345cadbdacd0004a4c5441
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-11-29 20:11:28 +0500

    Task4

---
+++ Tyuiu.DmitrievLR.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.DmitrievLR.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.DmitrievLR.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Addiction
+            Console.WriteLine(DataService.Addiction(1, 5));
+            //Substraction
+            Console.WriteLine(DataService.Substraction(5,1));
+            //Multiplication
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            //Division
+            Console.WriteLine(DataService.Division(10, 5));
+            Console.ReadKey();
+        }
+    }
+}
